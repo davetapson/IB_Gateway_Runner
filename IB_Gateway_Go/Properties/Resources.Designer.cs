e0485a15f88c08fb6d8e8204b67273a0bb9445cf
davetapson <dave.tapson@gmail.com>
2018-01-08 23:08:15 +0200

    Publish updates

--- IB_Gateway_Go/Properties/Resources.Designer.cs
+++ IB_Gateway_Go/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace IB_Gateway_Runner.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("IB_Gateway_Go.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("IB_Gateway_Runner.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
